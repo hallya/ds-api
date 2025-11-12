2355cc10e7492c5f37ba359f8061138721fb26eb
Lucien Dulac <lucien.dulac@pennylane.com>
2025-11-12 01:17:10 +0100

    chore(cleanup): remove obsolete .js files

--- index.js
+++
@@ -1,6 +0,0 @@
-export * from './lib/auth.js';
-export * from './lib/tasks.js';
-export * from './lib/api.js';
-export * from './lib/cli.js';
-export { SynologyDS } from './lib/synology-ds.js';
-export { CLIHandler } from './lib/cli-handler.js';
